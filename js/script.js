b0a893738fe2450f5097f616e73d84ac0a1d70a9
Sumit Kumar Rai <4235912+sumit-kumar-rai@users.noreply.github.com>
2022-04-14 09:57:23 +0545

    Updated format

--- js/script.js
+++ js/script.js
@@ -19,30 +19,19 @@ $.getJSON("data/data.json").done(function (data) {
         var start_date_string = months[start_date.getMonth()] + ' ' + start_date.getFullYear().toString();
         var end_date_string = ((end_date.getTime() === today_date.getTime()) ? 'Present' : months[end_date.getMonth()] + ' ' + end_date.getFullYear().toString());
 
-        titles += '<small>'
-        titles += title_object["title_name"]
-        titles += '<span>'
-        titles += ' — ' + title_object["location"]
-        titles += '</span>'
-        titles += '</small>'
-        titles += '<div class="xp-date">' + start_date_string.toUpperCase() + ' – ' + end_date_string.toUpperCase() + '</div>'
+        titles += '<h3><strong>' + title_object["title_name"] + '</strong> — ' + title_object["location"]  + '</h3>'
+        titles += '<div class="highlight">' + start_date_string.toUpperCase() + ' – ' + end_date_string.toUpperCase() + '</div>'
       });
       $.each(experience_object["responsibilities"], function (responsibility_index, responsibility_desc) {
         responsibilities += '<li>' + responsibility_desc + '</li>'
       });
-      experiences += '<div class="xp-job">'
       experiences += titles
-      experiences += '</div>'
-      experiences += '<div class="xp-detail">'
-      experiences += '<ul>'
-      experiences += responsibilities
-      experiences += '</ul>'
+      experiences += '<div class="details">'
+      experiences += '<ul>' + responsibilities + '</ul>'
       experiences += '</div>'
     });
-    work_experiences += '<div class="xp-item">'
-    work_experiences += '<div class="xp-company">'
-    work_experiences += '<b>' + work_experience_object["company"] + '</b>'
-    work_experiences += '</div>'
+    work_experiences += '<div class="items">'
+    work_experiences += '<h2>' + work_experience_object["company"] + '</h2>'
     work_experiences += experiences
     work_experiences += '</div>'
   });
@@ -60,43 +49,29 @@ $.getJSON("data/data.json").done(function (data) {
         var start_date_string = months[start_date.getMonth()] + ' ' + start_date.getFullYear().toString();
         var end_date_string = ((end_date.getTime() === today_date.getTime()) ? 'Present' : months[end_date.getMonth()] + ' ' + end_date.getFullYear().toString());
 
-        studies += '<small>'
-        studies += study_object["study_name"]
-        studies += '<span>'
-        studies += ' — ' + study_object["location"]
-        studies += '</span>'
-        studies += '</small>'
-        studies += '<div class="xp-date">' + start_date_string.toUpperCase() + ' – ' + end_date_string.toUpperCase() + '</div>'
+        studies += '<h3><strong>' + study_object["study_name"] + '</strong> — ' + study_object["location"]  + '</h3>'
+        studies += '<div class="highlight">' + start_date_string.toUpperCase() + ' – ' + end_date_string.toUpperCase() + '</div>'
       });
       $.each(education_object["responsibilities"], function (responsibility_index, responsibility_desc) {
         responsibilities += '<li>' + responsibility_desc + '</li>'
       });
-      educations += '<div class="xp-education">'
       educations += studies
-      educations += '</div>'
     });
-    academic_qualifications += '<div class="xp-item">'
-    academic_qualifications += '<div class="xp-institution">'
-    academic_qualifications += academic_qualification_object["institution"]
-    academic_qualifications += '</div>'
+    academic_qualifications += '<div class="items">'
+    academic_qualifications += '<h2>' + academic_qualification_object["institution"] + '</h2>'
     academic_qualifications += educations
     academic_qualifications += '</div>'
   });
 
   $.each(data["skills"], function (skill_name, skill_object) {
-    skills += '<div class="extra">'
-    skills += '<div class="extra-info">'
-    skills += skill_name
-    skills += '<br />'
-    skills += "<small>"
+    skills += '<h3>' + skill_name + '</h3>'
+    skills += '<div class="highlight">'
     skills += skill_object["sub_skills"].join(" · ").toUpperCase()
-    skills += '</small>'
     skills += '</div>'
     // skills += '<div class="extra-details">'
     // skills += '<div class="extra-details__progress" style="width: ' + skill_object["confidence"] + '%">'
     // skills += '</div>'
     // skills += '</div>'
-    skills += '</div>'
   });
 
   $("title").append(data["page"]);
