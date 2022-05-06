a0fed53f0869c5d1be47d573a526c96fdca317d6
Sumit Sampang Rai <4235912+sumit-sampang-rai@users.noreply.github.com>
2022-05-06 08:14:43 +0545

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,8 +11,7 @@ function date_to_string(date) {
 function calc_months(start, end) {
   var months = (end.getFullYear() - start.getFullYear()) * 12;
   months -= start.getMonth();
-  months += end.getMonth();
-  months += start.getDate() < end.getDate() ? 1 : 0
+  months += end.getMonth() + 1;
   return months;
 }
 
